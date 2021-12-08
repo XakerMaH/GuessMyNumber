4b2ffb7f5f827dbb5b73dbcfbde91b8c40dddb6c
XakerMaH <95735742+XakerMaH@users.noreply.github.com>
2021-12-08 10:37:17 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(static_cast<unsigned int>(time(0))); // запускаем генератор случайных чисел
+	int secretNumber = rand() % 15 + 1; //случайное число в диапазоне от 1до100
+	int tries = 0;
+	int guess;
+	cout << "\tWelcome to Guess My Number\n\n";
+
+	do
+	{
+		cout << "Enter a guess: ";
+		cin >> guess;
+		++tries;
+		if (guess > secretNumber)
+		{
+			cout << "Too high!\n\n";
+		}
+		else if (guess < secretNumber)
+		{
+			cout << "Too low!\n\n";
+		}
+		else
+		{
+			cout << "\nThat's it! You got it in " << tries << " guesses!\n";
+		}
+	} while (guess != secretNumber);
+	return 0;
+}
